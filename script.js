49639fa1b5c899833b0e93aaf4aa52b9063696c5
khushh1711 <shahkhush08@gmail.com>
2024-08-27 09:24:22 -0700

    first commit

--- script.js
+++ script.js
@@ -76,9 +76,9 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     let a = await fetch(`/songs/`)
-    let respose = await a.text();
+    let response = await a.text();
     let div = document.createElement("div")
-    div.innerHTML = respose;
+    div.innerHTML = response;
     let anchors = div.getElementsByTagName("a")
     let cardContainer = document.querySelector(".cardContainer")
     let array = Array.from(anchors)
@@ -86,13 +86,13 @@ async function displayAlbums() {
         const e = array[index];
         
 
-        if (e.href.includes("/songs")) {
+        if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = (e.href.split("/").slice(-2)[0])
 
             //Get the metadata of the folder
             let a = await fetch(`/songs/${folder}/info.json`)
-            let respose = await a.json();
-            console.log(respose)
+            let response = await a.json();
+            console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
                         <div class="play">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
@@ -103,8 +103,8 @@ async function displayAlbums() {
                         </div>
                         
                         <img src="/songs/${folder}/cover.jpg" alt="">
-                        <h2>${respose.title}</h2>
-                        <p>${respose.description}</p>
+                        <h2>${response.title}</h2>
+                        <p>${response.description}</p>
                     </div>`
         }
     }
